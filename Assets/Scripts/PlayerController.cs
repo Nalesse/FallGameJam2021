607bfa8bc33e11d00ab37fa0b21478601dcdbd08
Nalesse <nalesse@student.fitchburgstate.edu>
2021-10-20 19:45:33 -0400

    Refactored Player Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Android;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,25 +10,40 @@ public class PlayerController : MonoBehaviour
 
     public enum Elements
     {
+        /// <summary>
+        /// Burns obstacles
+        /// </summary>
         Fire,
+
+        /// <summary>
+        /// To be Determined
+        /// </summary>
         Wind,
+
+        /// <summary>
+        /// Freezes water
+        /// </summary>
         Ice,
-        Water,
+
+        /// <summary>
+        /// Shoots platforms
+        /// </summary>
         Earth
     }
 
+    public Elements CurrentElement { get; set; }
 
     #endregion
 
     #region Privite Variables
     private Rigidbody2D playerRb;
     private bool isGrounded;
+    private bool canJump;
     #endregion
 
     #region Inspector Fields
     [SerializeField] private float jumpForce;
     [SerializeField] private float speed;
-    [SerializeField] private Elements currentElement;
     #endregion
 
 
@@ -37,45 +53,55 @@ public class PlayerController : MonoBehaviour
         playerRb = transform.GetComponent<Rigidbody2D>();
     }
 
-    // Start is called before the first frame update
-    private void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     private void Update()
     {
         PlayerInput();
     }
 
+    private void FixedUpdate()
+    {
+        // Jump action is separated so that it gets handled by FixedUpdate and not Update which is more performant
+        Jump();
+    }
+
+    #region Player Movement
 
     private void PlayerInput()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector2.right * Time.deltaTime * speed);
-        }
+        // Moves the player left or right based off of the horizontal input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * Time.deltaTime * speed * horizontalInput);
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector2.left * Time.deltaTime * speed);
-        }
-
+        // if the player is not on the ground the rest of the logic does not get executed
         if (!isGrounded)
         {
             return;
         }
 
+        // Activates the jump function if the player presses "space" or "W".  
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
         {
-            playerRb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
-            isGrounded = false;
+            canJump = true;
         }
+    }
+
+    private void Jump()
+    {
+        // if canJump is false then the rest of the code does not get executed 
+        if (!canJump)
+        {
+            return;
+        }
+
+        playerRb.AddForce(this.transform.up * this.jumpForce, ForceMode2D.Impulse);
+        isGrounded = false;
+        canJump = false;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         isGrounded = true;
     }
+    #endregion
 }
